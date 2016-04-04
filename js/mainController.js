116195343ebe4870a99c04b346c57e8b24a3b975
Keith Moore <keith.m4@gmail.com>
2016-04-04 19:37:56 -0400

    init commit
    first draft of my portfolio written using material design

---
+++ js/mainController.js
@@ -0,0 +1,25 @@
+var portfolio = angular.module('portfolio', ['ngRoute']);
+
+portfolio.config(function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'pages/about.html',
+		controller: 'mainController'
+	});
+	$routeProvider.when('/portfolio',{
+		templateUrl: 'pages/portfolio.html',
+		controller: 'mainController'
+	});
+	$routeProvider.when('/contact',{
+		templateUrl: 'pages/contact.html',
+		controller: 'mainController'
+	});
+	// send the user back to the home page if the route is not valid
+	$routeProvider.otherwise({
+		templateUrl: 'pages/about.html',
+		controller: 'mainController'
+	});
+})
+
+portfolio.controller('mainController', function($scope){
+	// main controller
+})
