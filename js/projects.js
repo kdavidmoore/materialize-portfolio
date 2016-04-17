c45ac713f7ac7102e4ad49e9d68022cf719c5c48
Keith Moore <keith.m4@gmail.com>
2016-04-17 17:38:13 -0400

    added languages app and new project cards

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,13 @@
 // constructor for the portfolio cards
-function Card(title, src, description){
+function Card(title, src, description, demo){
 	this.title = title;
 	this.src = src + '.png';
 	this.description = description;
+	this.demo = demo;
 }
 // create an array and push each card to the array using the constructor
 var cards = [];
-cards.push(new Card("Search Places","maps","Search for different types of professional services in several major U.S. cities."));
-cards.push(new Card("Movie Crawler","movies","See what's playing and search for movies, TV shows, and actors."));
+cards.push(new Card("Languages of the World","langs","Filtering countries of the world by major languages spoken, total population, and GDP.","/languages")
+cards.push(new Card("Search Places","maps","Search for different types of professional services in several major U.S. cities.","/search-places"));
+cards.push(new Card("Movie Crawler","movies","See what's playing and search for movies, TV shows, and actors.","/movie-crawler"));
+cards.push(new Card("Calculator","calc","A basic calculator written in JavaScript.","/calculator"));
