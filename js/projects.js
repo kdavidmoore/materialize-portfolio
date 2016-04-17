57d3c81ecdf1548b9699f48e7665b5219eb279e1
Keith Moore <keith.m4@gmail.com>
2016-04-17 17:42:04 -0400

    typo

--- js/projects.js
+++ js/projects.js
@@ -10,5 +10,5 @@ var cards = [];
 cards.push(new Card("Languages of the World","langs","Filtering countries of the world by major languages spoken, total population, and GDP.","/languages"));
 cards.push(new Card("Search Places","maps","Search for different types of professional services in several major U.S. cities.","/search-places"));
 cards.push(new Card("Movie Crawler","movies","See what's playing and search for movies, TV shows, and actors.","/movie-crawler"));
-cards.push(new Card("Panama Papers Tweets","tweets.png","Get the latest scoop on the Panama Papers leaks.","/panama-papers"));
+cards.push(new Card("Panama Papers Tweets","tweets","Get the latest scoop on the Panama Papers leaks.","/panama-papers"));
 cards.push(new Card("Calculator","calc","A basic calculator written in JavaScript.","/calculator"));
