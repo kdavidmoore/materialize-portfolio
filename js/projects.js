77af3665f230013c1cded1d5e1ef144e01506bd8
Keith Moore <keith.m4@gmail.com>
2016-04-20 15:33:15 -0400

    added new devicons, re-organized skills section, adjusted section height, attempted php mail form

--- js/projects.js
+++ js/projects.js
@@ -8,7 +8,7 @@ function Card(title, src, description, demo){
 // create an array and push each card to the array using the constructor
 var cards = [];
 cards.push(new Card("Languages of the World","langs","Filtering countries of the world by major languages spoken, total population, and GDP.","http://kdavidmoore.com/languages"));
-cards.push(new Card("Chat Room","chat","A basic chat room built using Node.js.","https://shielded-fortress-12118.herokuapp.com/"));
+cards.push(new Card("Chat Room","chat","A basic chat room built using Node.js. Each new tab or window opens a new chat client.","https://shielded-fortress-12118.herokuapp.com/"));
 cards.push(new Card("Search Places","maps","Search for different types of professional services in several major U.S. cities.","http://kdavidmoore.com/search-places"));
 cards.push(new Card("Movie Crawler","movies","See what's playing and search for movies, TV shows, and actors.","http://kdavidmoore.com/movie-crawler"));
 cards.push(new Card("Panama Papers Tweets","tweets","Get the latest scoop on the Panama Papers leaks.","http://kdavidmoore.com/panama-papers"));
