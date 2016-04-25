58ccd94b70f0842f3ffa4f907b9a352e46333f54
Keith Moore <keith.m4@gmail.com>
2016-04-25 09:00:52 -0400

    used materializecss to add fixed card heights
    fixed bug where lowest card was getting cut off

--- js/projects.js
+++ js/projects.js
@@ -7,7 +7,7 @@ function Card(title, src, description, demo){
 }
 // create an array and push each card to the array using the constructor
 var cards = [];
-cards.push(new Card("Languages of the World","langs","Pulls data from JSON files and filters by each category using AngularJS and DataMaps.","http://kdavidmoore.com/languages"));
+cards.push(new Card("Interactive Map","langs","Pulls data from JSON files and filters by each category using AngularJS and DataMaps.","http://kdavidmoore.com/languages"));
 cards.push(new Card("Chat Room","chat","A basic chat room built using Node.js. Each new tab or window opens a new chat client.","https://shielded-fortress-12118.herokuapp.com/"));
 cards.push(new Card("Search Places","maps","Displays information about places using Google Maps API and AngularJS routing.","http://kdavidmoore.com/search-places"));
 cards.push(new Card("Movie Crawler","movies","Dynamically pulls data from themoviedb.org using AJAX and displays results using Isotope filtering.","http://kdavidmoore.com/movie-crawler"));
